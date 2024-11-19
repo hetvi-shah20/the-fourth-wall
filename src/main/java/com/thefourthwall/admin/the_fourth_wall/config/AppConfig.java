22a8082e7bd64afaa0339b11b322c18b47415d3f
Hetvi shah <43674111+hetvi-shah20@users.noreply.github.com>
2024-11-19 15:52:32 -0500

    cross-headers allowed

--- src/main/java/com/thefourthwall/admin/the_fourth_wall/config/AppConfig.java
+++ src/main/java/com/thefourthwall/admin/the_fourth_wall/config/AppConfig.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class AppConfig {
 
     @Bean
@@ -14,6 +16,7 @@ public class AppConfig {
         return new RestTemplate();
     }
 
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
