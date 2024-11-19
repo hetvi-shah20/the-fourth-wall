3ae42c27534ef1d4e3e0c1378a5a61a3994e430b
Hetvi shah <43674111+hetvi-shah20@users.noreply.github.com>
2024-11-19 15:33:42 -0500

    cross-origin allowed

--- src/main/java/com/thefourthwall/admin/the_fourth_wall/config/AppConfig.java
+++ src/main/java/com/thefourthwall/admin/the_fourth_wall/config/AppConfig.java
@@ -3,6 +3,8 @@ package com.thefourthwall.admin.the_fourth_wall.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://the4th-wall-admin.vercel.app/");
+            }
+        };
     }
 }
