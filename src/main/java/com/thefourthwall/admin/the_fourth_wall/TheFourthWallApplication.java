982d747262aa8a809c2028f77720b24cc83a0d92
Hetvi shah <43674111+hetvi-shah20@users.noreply.github.com>
2024-11-19 14:06:10 -0500

    intial commit

---
+++ src/main/java/com/thefourthwall/admin/the_fourth_wall/TheFourthWallApplication.java
@@ -0,0 +1,13 @@
+package com.thefourthwall.admin.the_fourth_wall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFourthWallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFourthWallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thefourthwall/admin/the_fourth_wall/TheFourthWallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thefourthwall.admin.the_fourth_wall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFourthWallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
